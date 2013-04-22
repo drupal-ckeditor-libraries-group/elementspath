ff828449b6fa8942bb4205f55c997123ca61c053
Wiktor Walc <w.walc@cksource.com>
2013-04-22 16:29:28 +0200

    Updated language files, added Albanian translation.

--- lang/sq.js
+++ lang/sq.js
@@ -2,7 +2,7 @@
 Copyright (c) 2003-2013, CKSource - Frederico Knabben. All rights reserved.
 For licensing, see LICENSE.html or http://ckeditor.com/license
 */
-CKEDITOR.plugins.setLang( 'elementspath', 'fr-ca', {
-	eleLabel: 'Chemin d\'éléments',
-	eleTitle: 'element %1'
+CKEDITOR.plugins.setLang( 'elementspath', 'sq', {
+	eleLabel: 'Rruga e elementeve',
+	eleTitle: '%1 element'
 });
